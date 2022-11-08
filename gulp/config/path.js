5ffdea176b0db5e49a270ebb533b5af7fa8a0327
ivandmitrienko <oneblackcrystal@gmail.com>
2022-11-08 18:10:12 +0300

    initial commit

---
+++ gulp/config/path.js
@@ -0,0 +1,37 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./dist`;
+const srcFolder = `./src`;
+
+export const path = {
+    build: {
+        js: `${buildFolder}/js/`,
+        css: `${buildFolder}/css/`,
+        html:`${buildFolder}/`,
+        files:`${buildFolder}/files/`,
+        images:`${buildFolder}/img/`,
+        fonts:`${buildFolder}/fonts/`,
+    },
+    src: {
+        images: `${srcFolder}/img/**/*.{jpeg,jpg,png,gif,webp}`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        js: `${srcFolder}/js/app.js`,
+        scss: `${srcFolder}/scss/style.scss`,
+        html: `${srcFolder}/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
+        svgicons: `${srcFolder}/svgicons/*.svg`,
+    },
+    watch:{
+        js: `${srcFolder}/js/**/*.js`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        html: `${srcFolder}/**/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
+        images: `${srcFolder}/img/**/*.{jpeg,jpg,png,gif,webp}`,
+    },
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+    ftp:``
+}
