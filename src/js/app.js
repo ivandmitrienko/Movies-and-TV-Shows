22511c9eef82caf77d2013abeb5090304ec81851
ivandmitrienko <oneblackcrystal@gmail.com>
2022-11-10 22:19:29 +0300

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,24 @@
-import * as allfunctions from './modules/functions.js';
+// import * as allfunctions from './modules/functions.js';
+'use strict'
+
+const signUpToAccount = document.querySelector('.create-account__sign-up');
+const questionOfAuthentification = document.querySelector('.create-account__question');
+const titleOfenterToAccount = document.getElementsByTagName('h1')[0];
+const buttonOfenterToAccount = document.getElementsByTagName('button')[0];
+const repeatPassword = document.querySelector('.authentication-password-second');
+
+signUpToAccount.addEventListener('click', () => {
+    if (signUpToAccount.innerHTML == 'Sign Up') {
+        signUpToAccount.innerHTML = 'Login';
+        titleOfenterToAccount.innerHTML = 'Sign Up';
+        buttonOfenterToAccount.innerHTML = 'Create an account';
+        questionOfAuthentification.innerHTML = 'Already have an account?';
+        repeatPassword.className = 'authentication-password-second--active';
+    } else {
+        signUpToAccount.innerHTML = 'Sign Up';
+        titleOfenterToAccount.innerHTML = 'Login';
+        buttonOfenterToAccount.innerHTML = 'Login to your account';
+        questionOfAuthentification.innerHTML = 'Don’t have an account?';
+        repeatPassword.className = 'authentication-password-second';
+    }
+});
