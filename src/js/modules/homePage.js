3881c1a2b825e296b3252f25bc99330f0caeea8c
ivandmitrienko <oneblackcrystal@gmail.com>
2022-11-20 01:02:06 +0300

    initial commit

--- src/js/modules/homePage.js
+++ src/js/modules/homePage.js
@@ -3,11 +3,38 @@
 const content = document.getElementById('content-video');
 const homePage = document.createElement('div');
 homePage.className = 'homePage';
+content.appendChild(homePage);
+
 const trending = document.createElement('div');
 trending.className = 'trending';
+homePage.appendChild(trending);
+const trendingTitle = document.createElement('h1');
+trendingTitle.innerText = 'Trending'
+trending.appendChild(trendingTitle);
+const trendingItems = document.createElement('div');
+trendingItems.className = 'trending__items';
+trending.appendChild(trendingItems);
+
+function getTrendingItemsBE(result) {
+    result.forEach((item)=>{
+        let trendingItem = document.createElement('div');
+        trendingItem.innerText = item.title;
+        return trendingItems.appendChild(trendingItem);
+    })
+}
+
+const trendingItemsBE = () => {
+    fetch(`http://localhost:3001/trending`)
+        .then(res => res.json())
+        .then(data => getTrendingItemsBE(data))
+};
+
+trendingItemsBE();
 
 
-content.appendChild(homePage);
+
+
+// console.log(resultTrendingBE);
 
 
 
