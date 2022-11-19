7a2f3e1699500d82469209c3e3bd21e91582a53c
ivandmitrienko <oneblackcrystal@gmail.com>
2022-11-19 22:05:16 +0300

    initial commit

--- src/js/modules/homePage.js
+++ src/js/modules/homePage.js
@@ -1,1 +1,22 @@
 'use strict'
+
+const content = document.getElementById('content-video');
+const homePage = document.createElement('div');
+homePage.className = 'homePage';
+const trending = document.createElement('div');
+trending.className = 'trending';
+
+
+content.appendChild(homePage);
+
+
+
+
+
+
+// onclick="{
+//     fetch(`http://localhost:3001/trending`)
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+
+// }"
