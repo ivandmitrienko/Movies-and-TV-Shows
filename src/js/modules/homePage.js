8d60195b03ef5d451d17cb4864c4772d811516c2
ivandmitrienko <oneblackcrystal@gmail.com>
2022-11-21 23:47:27 +0300

    initial commit

--- src/js/modules/homePage.js
+++ src/js/modules/homePage.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import svgIcon from './bookmarkIconSvg.js';
+
 const content = document.getElementById('content-video');
 const homePage = document.createElement('div');
 homePage.className = 'homePage';
@@ -19,7 +21,11 @@ function getTrendingItemsBE(result) {
     result.forEach((item)=>{
         let trendingItem = document.createElement('div');
         trendingItem.className = 'trending-items__item'; 
-        trendingItem.style.backgroundImage = `url(${item.image})`;
+        trendingItem.style.background = `url(${item.image}) 0 0/470px 230px no-repeat`;
+        let trendingItemBookmark = document.createElement('div');
+        trendingItemBookmark.className = 'trending-items__item-bookmarket';
+        let trendingItemBookmarkIcon = svgIconBookmarket; 
+        trendingItem.appendChild(trendingItemBookmark);
         return trendingItems.appendChild(trendingItem);
     })
 }
