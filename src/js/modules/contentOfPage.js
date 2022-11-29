5efa712974801f868141525045e3aed13f9b786a
ivandmitrienko <oneblackcrystal@gmail.com>
2022-11-30 00:22:50 +0300

    inital commit

--- src/js/modules/contentOfPage.js
+++ src/js/modules/contentOfPage.js
@@ -4,8 +4,8 @@ export function contentOfPage() {
     // const keyBE = history.state.firstKey;
     const keyBE = "trending"; // for dev first key from history
     const titleBE = "trending" // for dev second key from history
-    // const allContentBE = "recommended"; // for dev first key from history
-    // const titleContentBE = "recommended for you"; // for dev second key from history
+    const allContentBE = "recommended"; // for dev first key from history
+    const titleContentBE = "recommended for you"; // for dev second key from history
     const mainContent = document.getElementById('mainContent');
     const page = document.createElement('div');
     page.className = 'page';
@@ -17,9 +17,9 @@ export function contentOfPage() {
 
     BEfunction(keyBE, titleBE, page_content);
 
-    // if(allContentBE) {
-    //     BEfunction(allContentBE, titleContentBE, page_content);
-    // }
+    if(allContentBE) {
+        BEfunction(allContentBE, titleContentBE, page_content);
+    }
 
     return mainContent;
 }
