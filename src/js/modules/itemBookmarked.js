6a262ffa7cc3d61572709e1e3e7fff75f6e11c5c
ivandmitrienko <oneblackcrystal@gmail.com>
2022-12-07 00:18:56 +0300

    initial commit

--- src/js/modules/itemBookmarked.js
+++ src/js/modules/itemBookmarked.js
@@ -1,7 +1,11 @@
 "use strict"
 
-export function itemBookmarked (EO) {
-    let el = EO.target;
-    let arrOfClick = document
+import { contentOfPage } from './contentOfPage.js';
 
+export function itemBookmarked(el) {
+    const arrKeyFromStorage = Object.keys(localStorage);
+    if (arrKeyFromStorage.find(i => i == el.id)) {
+        delete localStorage[el.id];
+    } else localStorage.setItem(el.id, JSON.stringify(el));
+    contentOfPage();
 }
