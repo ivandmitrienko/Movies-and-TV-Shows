6023d253c161a8d72456319487fe00094ff47eda
ivandmitrienko <oneblackcrystal@gmail.com>
2022-12-06 17:39:25 +0300

    initial commit

--- src/js/modules/authentification.js
+++ src/js/modules/authentification.js
@@ -1,133 +1,133 @@
 import { contentOfPage } from './contentOfPage.js';
 
-window.addEventListener('popstate', () =>{
-    contentOfPage();
-}) // need thinking
+// window.addEventListener('popstate', () =>{
+//     contentOfPage();
+// }) // need thinking
 
-const userAuthentification = document.querySelector('.user-authentification');
+// const userAuthentification = document.querySelector('.user-authentification');
 
-if (document.location.href === 'http://localhost:3000/') {
-    let temp = document.getElementsByTagName("template")[0];
-    let clon = temp.content.cloneNode(true);
-    userAuthentification.appendChild(clon);
-    const form = document.querySelector('.authentication');
-    const fields = form.querySelectorAll('.field');
-    const errorsInFields = Array.from(fields);
-    const errorsInSpans = document.querySelectorAll('.errorInSpan');
-    const signUpToAccount = document.querySelector('.create-account__sign-up');
-    const questionOfAuthentification = document.querySelector('.create-account__question');
-    const titleOfenterToAccount = document.getElementsByTagName('h1')[0];
-    const buttonOfenterToAccount = document.querySelector('.authentication-button__submit');
-    const repeatPassword = document.querySelector('.authentication-password-second');
-    const root = document.getElementById('root');
-    let arrFocus = []; // for errors
+// if (document.location.href === 'http://localhost:3000/') {
+//     let temp = document.getElementsByTagName("template")[0];
+//     let clon = temp.content.cloneNode(true);
+//     userAuthentification.appendChild(clon);
+//     const form = document.querySelector('.authentication');
+//     const fields = form.querySelectorAll('.field');
+//     const errorsInFields = Array.from(fields);
+//     const errorsInSpans = document.querySelectorAll('.errorInSpan');
+//     const signUpToAccount = document.querySelector('.create-account__sign-up');
+//     const questionOfAuthentification = document.querySelector('.create-account__question');
+//     const titleOfenterToAccount = document.getElementsByTagName('h1')[0];
+//     const buttonOfenterToAccount = document.querySelector('.authentication-button__submit');
+//     const repeatPassword = document.querySelector('.authentication-password-second');
+//     const root = document.getElementById('root');
+//     let arrFocus = []; // for errors
 
-    signUpToAccount.addEventListener('click', () => {
-        if (signUpToAccount.innerHTML == 'Sign Up') {
-            signUpToAccount.innerHTML = 'Login';
-            titleOfenterToAccount.innerHTML = 'Sign Up';
-            buttonOfenterToAccount.value = 'Create an account';
-            questionOfAuthentification.innerHTML = 'Already have an account?';
-            repeatPassword.className = 'authentication-password-second--active';
-        } else {
-            signUpToAccount.innerHTML = 'Sign Up';
-            titleOfenterToAccount.innerHTML = 'Login';
-            buttonOfenterToAccount.value = 'Login to your account';
-            questionOfAuthentification.innerHTML = 'Don’t have an account?';
-            repeatPassword.className = 'authentication-password-second';
-        }
-    });
+//     signUpToAccount.addEventListener('click', () => {
+//         if (signUpToAccount.innerHTML == 'Sign Up') {
+//             signUpToAccount.innerHTML = 'Login';
+//             titleOfenterToAccount.innerHTML = 'Sign Up';
+//             buttonOfenterToAccount.value = 'Create an account';
+//             questionOfAuthentification.innerHTML = 'Already have an account?';
+//             repeatPassword.className = 'authentication-password-second--active';
+//         } else {
+//             signUpToAccount.innerHTML = 'Sign Up';
+//             titleOfenterToAccount.innerHTML = 'Login';
+//             buttonOfenterToAccount.value = 'Login to your account';
+//             questionOfAuthentification.innerHTML = 'Don’t have an account?';
+//             repeatPassword.className = 'authentication-password-second';
+//         }
+//     });
 
-    form.addEventListener('submit', (EO) => {
-        EO.preventDefault();
-        if (signUpToAccount.innerHTML === 'Sign Up') {
-            return alert('You need to create account!');
-        }
-        arrFocus.length = 0;
-        let caughtErrorsInSpans = Array.from(errorsInSpans);
-        let errors = form.querySelectorAll('.error');
-        let caughtErrors = Array.from(errors);
-        for (let i = 0; i < caughtErrors.length; i++) {
-            if (!errorsInFields[i].value) {
-                caughtErrors[i].innerHTML = 'empty field';
-                arrFocus.push(errorsInFields[i]);
-            } else if (caughtErrorsInSpans[i].innerHTML) {
-                arrFocus.push(errorsInFields[i]);
-            }
-        }
-        if (arrFocus.length) {
-            arrFocus[0].focus();
-        } else {
-            userAuthentification.style.display = 'none';
-            root.style.display = 'block';
-            history.pushState(
-                {
-                    firstKey: "trending",
-                    secondKey: "recommended",
-                    secondKeyTitle: "recommended for you"
+//     form.addEventListener('submit', (EO) => {
+//         EO.preventDefault();
+//         if (signUpToAccount.innerHTML === 'Sign Up') {
+//             return alert('You need to create account!');
+//         }
+//         arrFocus.length = 0;
+//         let caughtErrorsInSpans = Array.from(errorsInSpans);
+//         let errors = form.querySelectorAll('.error');
+//         let caughtErrors = Array.from(errors);
+//         for (let i = 0; i < caughtErrors.length; i++) {
+//             if (!errorsInFields[i].value) {
+//                 caughtErrors[i].innerHTML = 'empty field';
+//                 arrFocus.push(errorsInFields[i]);
+//             } else if (caughtErrorsInSpans[i].innerHTML) {
+//                 arrFocus.push(errorsInFields[i]);
+//             }
+//         }
+//         if (arrFocus.length) {
+//             arrFocus[0].focus();
+//         } else {
+//             userAuthentification.style.display = 'none';
+//             root.style.display = 'block';
+//             history.pushState(
+//                 {
+//                     firstKey: "trending",
+//                     secondKey: "recommended",
+//                     secondKeyTitle: "recommended for you"
 
-                },
-                '',
-                'home'
-            )
-            contentOfPage();
-        }
-    })
+//                 },
+//                 '',
+//                 'home'
+//             )
+//             contentOfPage();
+//         }
+//     })
 
-    function borderRedColor(el, color) {
-        el.style.borderColor = color;
-    };
-
-
-    errorsInFields.forEach((v) => {
-        v.onblur = () => {
-            if (v.type === 'email') {
-                let error = document.querySelector('.emailSpan');
-                if (!(v.value)) {
-                    error.innerHTML = 'empty field';
-                    borderRedColor(v, '#FC4747');
-                } else if (!v.value.endsWith('@gmail.com')) {
-                    error.innerHTML = 'Please, add gmail.com!';
-                    borderRedColor(v, '#FC4747');
-                } else {
-                    error.innerHTML = '';
-                    borderRedColor(v, '#5A698F');
-                }
-            }
-            if (v.placeholder === 'Password') {
-                let error = document.querySelector('.firstPasswordSpan');
-                if (!(v.value)) {
-                    error.innerHTML = 'empty field';
-                    borderRedColor(v, '#FC4747');
-                } else if (v.value.length < 8) {
-                    error.innerHTML = 'min 8 symbols';
-                    borderRedColor(v, '#FC4747');
-                } else {
-                    error.innerHTML = '';
-                    borderRedColor(v, '#5A698F');
-                }
-            }
-            if (v.placeholder === 'Repeat password') {
-                let error = document.querySelector('.secondPasswordSpan');
-                let userPassword = document.getElementById('userPassword');
-                if (!(v.value)) {
-                    error.innerHTML = 'empty field';
-                    borderRedColor(v, '#FC4747');
-                } else if (userPassword.value != v.value) {
-                    error.innerHTML = 'check your password';
-                    borderRedColor(v, '#FC4747');
-                } else {
-                    error.innerHTML = '';
-                    borderRedColor(v, '#5A698F');
-                }
-            }
-        }
-    })
+//     function borderRedColor(el, color) {
+//         el.style.borderColor = color;
+//     };
 
 
-} else {
-    userAuthentification.innerHTML='';
-    root.style.display = 'block';
-    contentOfPage();
-}
+//     errorsInFields.forEach((v) => {
+//         v.onblur = () => {
+//             if (v.type === 'email') {
+//                 let error = document.querySelector('.emailSpan');
+//                 if (!(v.value)) {
+//                     error.innerHTML = 'empty field';
+//                     borderRedColor(v, '#FC4747');
+//                 } else if (!v.value.endsWith('@gmail.com')) {
+//                     error.innerHTML = 'Please, add gmail.com!';
+//                     borderRedColor(v, '#FC4747');
+//                 } else {
+//                     error.innerHTML = '';
+//                     borderRedColor(v, '#5A698F');
+//                 }
+//             }
+//             if (v.placeholder === 'Password') {
+//                 let error = document.querySelector('.firstPasswordSpan');
+//                 if (!(v.value)) {
+//                     error.innerHTML = 'empty field';
+//                     borderRedColor(v, '#FC4747');
+//                 } else if (v.value.length < 8) {
+//                     error.innerHTML = 'min 8 symbols';
+//                     borderRedColor(v, '#FC4747');
+//                 } else {
+//                     error.innerHTML = '';
+//                     borderRedColor(v, '#5A698F');
+//                 }
+//             }
+//             if (v.placeholder === 'Repeat password') {
+//                 let error = document.querySelector('.secondPasswordSpan');
+//                 let userPassword = document.getElementById('userPassword');
+//                 if (!(v.value)) {
+//                     error.innerHTML = 'empty field';
+//                     borderRedColor(v, '#FC4747');
+//                 } else if (userPassword.value != v.value) {
+//                     error.innerHTML = 'check your password';
+//                     borderRedColor(v, '#FC4747');
+//                 } else {
+//                     error.innerHTML = '';
+//                     borderRedColor(v, '#5A698F');
+//                 }
+//             }
+//         }
+//     })
+
+
+// } else {
+//     userAuthentification.innerHTML='';
+//     root.style.display = 'block';
+//     contentOfPage();
+// }
 
--- src/js/modules/contentOfPage.js
+++ src/js/modules/contentOfPage.js
@@ -4,10 +4,14 @@ import { svgFunc } from './svgIconFunctions.js';
 import { BEfunction } from "./BEfunction.js";
 
 export function contentOfPage() { 
-    const keyBE = history.state.firstKey;
-    const titleBE = keyBE;
-    const allContentBE = history.state.secondKey; 
-    const titleContentBE = history.state.secondKeyTitle; 
+    // const keyBE = history.state.firstKey;
+    // const titleBE = keyBE;
+    const keyBE = "trending";
+    const titleBE = "trending";
+    // const allContentBE = history.state.secondKey; 
+    const allContentBE = "recommended"; 
+    // const titleContentBE = history.state.secondKeyTitle;
+    const titleContentBE =  "recommended for you"; 
     const mainContent = document.getElementById('mainContent');
     mainContent.innerHTML = '';
     const page = document.createElement('div');
@@ -29,3 +33,5 @@ export function contentOfPage() {
 
     return mainContent;
 }
+
+contentOfPage();
