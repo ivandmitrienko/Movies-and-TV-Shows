582db1928fbdb4be1232f8a194d41d016346e75c
ivandmitrienko <oneblackcrystal@gmail.com>
2022-12-10 00:31:10 +0300

    initial commit

--- src/js/modules/showBookmarkedItems.js
+++ src/js/modules/showBookmarkedItems.js
@@ -1,8 +1,10 @@
 "use strict"
 
-const arrOfItems = Object.values(localStorage);
+import { showItemsBE } from "./showItemsBE.js";
+
 
 export function showBookmarkedItems (){
+    const arrOfBookmarkedItems = Object.values(localStorage);
     const bookmarkedIcon = document.querySelector(".Bookmarked");
     bookmarkedIcon.addEventListener("click", ()=>{
         const page_content = document.querySelector(".page-content")
@@ -10,10 +12,19 @@ export function showBookmarkedItems (){
         const title = document.createElement('h1');
         title.innerText = "Bookmarked Movies";
         page_content.appendChild(title);
-        
-        for(let i=0; i<arrOfItems.length; i++){
-            console.log(JSON.parse(arrOfItems[i]));
+        const items = document.createElement('div');
+        items.className = 'items';
+        items.classList.add(`items--bookmarked`);
+        page_content.appendChild(items);
+        const arrOfItems = [];
+        for(let i=0; i<arrOfBookmarkedItems.length; i++){
+            arrOfItems.push(JSON.parse(arrOfBookmarkedItems[i]));
         }
+        showItemsBE(arrOfItems, items);
+        
+        // for(let i=0; i<arrOfItems.length; i++){
+        //     console.log(JSON.parse(arrOfItems[i]));
+        // }
 
     })
 }
