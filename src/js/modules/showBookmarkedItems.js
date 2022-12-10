20b9f86b6b848ce740cb92308e8f8c27cdb50aac
ivandmitrienko <oneblackcrystal@gmail.com>
2022-12-10 22:17:36 +0300

    initial commit

--- src/js/modules/showBookmarkedItems.js
+++ src/js/modules/showBookmarkedItems.js
@@ -2,26 +2,37 @@
 
 import { showItemsBE } from "./showItemsBE.js";
 
-
-export function showBookmarkedItems (){
+export function showBookmarkedItems() {
     const arrOfBookmarkedItems = Object.values(localStorage);
+    const arrOfItems = [];
+    for (let i = 0; i < arrOfBookmarkedItems.length; i++) {
+        arrOfItems.push(JSON.parse(arrOfBookmarkedItems[i]));
+    }
     const bookmarkedIcon = document.querySelector(".Bookmarked");
-    bookmarkedIcon.addEventListener("click", ()=>{
+    bookmarkedIcon.addEventListener("click", () => {
         const page_content = document.querySelector(".page-content")
-        page_content.innerHTML='';
-        const title = document.createElement('h1');
-        title.innerText = "Bookmarked Movies";
-        page_content.appendChild(title);
-        const items = document.createElement('div');
-        items.className = 'items';
-        items.classList.add(`items--bookmarked`);
-        page_content.appendChild(items);
-        const arrOfItems = [];
-        for(let i=0; i<arrOfBookmarkedItems.length; i++){
-            arrOfItems.push(JSON.parse(arrOfBookmarkedItems[i]));
+        page_content.innerHTML = '';
+        const arrOfMovie = arrOfItems.filter((i) => i.category === "Movie");
+        const arrOfTVSeries = arrOfItems.filter((i) => i.category === "TV Series");
+        if (arrOfMovie.length) {
+            const title = document.createElement('h1');
+            title.innerText = "Bookmarked Movies";
+            page_content.appendChild(title);
+            const items = document.createElement('div');
+            items.className = 'items';
+            items.classList.add(`items--bookmarked`);
+            page_content.appendChild(items);
+            showItemsBE(arrOfMovie, items);
         }
-        const arrOfItemsForCategory = arrOfItems.filter((i)=>i.category === "Movie");
-        showItemsBE(arrOfItemsForCategory, items);
-
+        if (arrOfTVSeries.length) {
+            const title = document.createElement('h1');
+            title.innerText = "Bookmarked TV Series";
+            page_content.appendChild(title);
+            const items = document.createElement('div');
+            items.className = 'items';
+            items.classList.add(`items--bookmarked`);
+            page_content.appendChild(items);
+            showItemsBE(arrOfTVSeries, items);
+        }
     })
 }
