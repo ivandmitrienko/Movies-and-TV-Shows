85516338a40c8fbd1d5a3856f0aa97e4f1eb2a28
ivandmitrienko <oneblackcrystal@gmail.com>
2022-12-17 00:18:14 +0300

    initial commit

--- src/js/modules/showVideo.js
+++ src/js/modules/showVideo.js
@@ -1,10 +1,17 @@
 "use strict"
 
 export function showVideo(i){
-    console.log(i.video)
     const content = document.querySelector('.content');
     const videoContainer = document.createElement('div');
     videoContainer.className=('content-video');
+    const closerVideoOfContainer = document.createElement('div');
+    closerVideoOfContainer.className = ('content-video__closer');
+    closerVideoOfContainer.innerHTML = "X";
+    videoContainer.appendChild(closerVideoOfContainer);
+    closerVideoOfContainer.addEventListener('click', ()=>{
+        content.removeChild(videoContainer);
+
+    })
     const video = document.createElement('iframe');
     video.setAttribute('src', `${i.video}`);
     videoContainer.appendChild(video);
